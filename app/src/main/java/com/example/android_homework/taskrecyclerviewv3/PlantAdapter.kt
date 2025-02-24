0f9c944c960f4580fb1b778cc63ce93fccc2c2d3
Serega1067 <serega1067@yandex.ru>
2025-02-24 18:42:48 +0700

    Adding adapter PlantAdapter

--- app/src/main/java/com/example/android_homework/taskrecyclerviewv3/PlantAdapter.kt
+++ app/src/main/java/com/example/android_homework/taskrecyclerviewv3/PlantAdapter.kt
@@ -1,13 +1,15 @@
 package com.example.android_homework.taskrecyclerviewv3
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.android_homework.taskrecyclerviewv3.databinding.PlantItemBinding
 
-class PlantAdapter : RecyclerView.Adapter<PlantAdapter.PlantHolder> {
+class PlantAdapter : RecyclerView.Adapter<PlantAdapter.PlantHolder>() {
 
+    val plantList = ArrayList<Plant>()
     class PlantHolder(item: View) : RecyclerView.ViewHolder(item) {
         val binding = PlantItemBinding.bind(item)
         fun bind(plant: Plant) = with(binding) {
@@ -20,14 +22,22 @@ class PlantAdapter : RecyclerView.Adapter<PlantAdapter.PlantHolder> {
         parent: ViewGroup,
         viewType: Int
     ) : PlantHolder {
-
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.plant_item, parent, false)
+        return PlantHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-
+    override fun onBindViewHolder(holder: PlantHolder, position: Int) {
+        holder.bind(plantList[position])
     }
 
     override fun getItemCount(): Int {
+        return plantList.size
+    }
 
+    fun addPlant(plant: Plant) {
+        plantList.add(plant)
+        notifyDataSetChanged()
     }
 }
