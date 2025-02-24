14dde95b4e32b6f801d5d4c045190f308e5f2524
Serega1067 <serega1067@yandex.ru>
2025-02-24 19:22:43 +0700

    MainActivity and task done

--- app/src/main/java/com/example/android_homework/taskrecyclerviewv3/MainActivity.kt
+++ app/src/main/java/com/example/android_homework/taskrecyclerviewv3/MainActivity.kt
@@ -5,16 +5,42 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.android_homework.taskrecyclerviewv3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    private val adapter = PlantAdapter()
+    private var index = 0
+    private val imageIdList = listOf(
+        R.drawable.plant1,
+        R.drawable.plant2,
+        R.drawable.plant3,
+        R.drawable.plant4,
+        R.drawable.plant5,
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        init()
+    }
+
+    private fun init() {
+        binding.apply {
+            rcView.layoutManager = GridLayoutManager(this@MainActivity, 3)
+            rcView.adapter = adapter
+            buttonAdd.setOnClickListener {
+                if (index > 4) index = 0
+//                val plant = Plant(imageIdList[index], "Plant $index")
+                adapter.addPlant(Plant(imageIdList[index], "Plant $index"))
+                index++
+            }
         }
     }
 }
