e566665ee9e49a240b15077296b970701f371b13
Serega1067 <serega1067@yandex.ru>
2025-02-25 10:56:24 +0700

    Template activity_edit done

--- app/src/main/java/com/example/android_homework/taskrecyclerviewv3/EditActivity.kt
+++ app/src/main/java/com/example/android_homework/taskrecyclerviewv3/EditActivity.kt
@@ -5,16 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.android_homework.taskrecyclerviewv3.databinding.ActivityEditBinding
 
 class EditActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityEditBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_edit)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityEditBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
